bea7f2267e06692eab820dee3517df19bb760210
heetshah15 <heet9000@gmail.com>
2025-08-24 18:27:06 -0400

    redo of repo

--- Projects/boilerplate-mean-variance-standard-deviation-calculator-main/main.py
+++ Projects/boilerplate-mean-variance-standard-deviation-calculator-main/main.py
@@ -2,7 +2,7 @@
 import mean_var_std
 from unittest import main
 
-for key, value in mean_var_std.calculate([0,1,2,3,4,5,6,7,8]).items():
+for key, value in mean_var_std.calculate([0, 1, 2, 3, 4, 5, 6, 7, 8]).items():
     print(f"{key}: {value}")
 
 
